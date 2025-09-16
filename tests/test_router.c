c1df47c86ccbf895555f7e2c80531c0a58b1e062
Omid Jaberi <omid.jaberie@gmail.com>
2025-09-16 21:43:25 +0330

    Update: Test Router with method

--- tests/test_router.c
+++ tests/test_router.c
@@ -11,30 +11,52 @@ void handler_root(Request *req, Response *res)
     set_body(res, "Root OK");
 }
 
-void handler_hello(Request *req, Response *res)
+void handler_hello_get(Request *req, Response *res)
 {
     init_response(res);
     set_header(res, "Content-Type", "text/plain");
-    set_body(res, "Hello OK");
+    set_body(res, "Hello GET OK");
 }
 
-void test_route_match()
+void handler_hello_post(Request *req, Response *res)
 {
-    add_route("/", handler_root);
-    add_route("/hello", handler_hello);
+    init_response(res);
+    set_header(res, "Content-Type", "text/plain");
+    set_body(res, "Hello POST OK");
+}
+
+void test_route_match_get()
+{
+    add_route("GET", "/", handler_root);
+    add_route("GET", "/hello", handler_hello_get);
 
     Request req;
     Response res;
+    strcpy(req.method, "GET");
     strcpy(req.path, "/hello");
 
     handle_request(&req, &res);
-    assert(strcmp(res.body, "Hello OK") == 0);
+    assert(strcmp(res.body, "Hello GET OK") == 0);
+}
+
+void test_route_match_post()
+{
+    add_route("POST", "/hello", handler_hello_post);
+
+    Request req;
+    Response res;
+    strcpy(req.method, "POST");
+    strcpy(req.path, "/hello");
+
+    handle_request(&req, &res);
+    assert(strcmp(res.body, "Hello POST OK") == 0);
 }
 
 void test_route_not_found()
 {
     Request req;
     Response res;
+    strcpy(req.method, "GET");
     strcpy(req.path, "/doesnotexist");
 
     handle_request(&req, &res);
@@ -43,7 +65,8 @@ void test_route_not_found()
 
 int main()
 {
-    test_route_match();
+    test_route_match_get();
+    test_route_match_post();
     test_route_not_found();
     printf("All Router tests passed.\n");
     return 0;
