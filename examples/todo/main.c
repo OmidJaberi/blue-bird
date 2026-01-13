fbc518009a8cd4a85b0b7eca14f30212ad679203
Omid Jaberi <omid.jaberie@gmail.com>
2026-01-13 22:54:36 +0330

    feat: sqlite persist

--- examples/todo/main.c
+++ examples/todo/main.c
@@ -3,6 +3,8 @@
 #include "core/http.h"
 #include "core/server.h"
 #include "log/console_logger.h"
+#include "persist/persist.h"
+#include "persist/persist_sqlite.h"
 
 int main()
 {
@@ -10,11 +12,21 @@ int main()
     logger_init_console(&console_logger, LOG_LEVEL_INFO, stderr);
     default_logger = console_logger;
 
+    const char *dbfile = "todo_sqlite.db";
+
+    /* Clean up from previous runs */
+    // unlink(dbfile);
+
+    /* Register backend */
+    persist_sqlite_register();
+    persist_set_default("sqlite");
+    persist_set_default_uri(dbfile);
+
     Server server;
     init_server(&server, 8080);
 
-    add_route(&server, "POST", "/:id/add_task", add_task);
-    add_route(&server, "GET", "/:id/list_tasks", list_tasks);
+    add_route(&server, "POST", "/add_task", add_task);
+    add_route(&server, "GET", "/list_tasks", list_tasks);
     
     start_server(&server);
     return 0;
--- examples/todo/src/app_handlers.c
+++ examples/todo/src/app_handlers.c
@@ -1,7 +1,17 @@
 #include "app_handlers.h"
+#include "log/log.h"
+
+#include "persist/persist.h"
+#include "persist/persist_sqlite.h"
 
 BBError add_task(request_t *req, response_t *res)
 {
+    http_message_t *msg = &GET_SERVER_REQUEST_MESSAGE(*req);
+    LOG_INFO("Add task: %s\n", msg->body);
+    if (persist_save(msg->body, "not_done", 8) != 0) {
+        printf("FAIL: persist_save\n");
+        return BB_ERROR(BB_ERR_BAD_REQUEST, "Failed to save.");
+    }
     return BB_SUCCESS();
 }
 
