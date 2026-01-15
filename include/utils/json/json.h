6fe1715844311e228c6937b8169a715a2348ba8e
Omid Jaberi <omid.jaberie@gmail.com>
2026-01-15 21:11:32 +0330

    feat: value union

--- include/utils/json/json.h
+++ include/utils/json/json.h
@@ -1,6 +1,8 @@
 #ifndef BB_JSON
 #define BB_JSON
 
+#include <stdbool.h>
+
 typedef enum {
     null,
     boolean,
@@ -13,6 +15,16 @@ typedef enum {
 
 typedef struct {
     json_node_type type;
+    int size; // For array type
+
+    union {
+        bool bool_val;
+        int int_val;
+        float real_val;
+        char *text_val;
+        json_node_t *array;
+        // map for object?
+    } value;
 } json_node_t;
 
 #endif //BB_JSON
