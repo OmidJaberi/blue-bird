0e5e835fba19c8ddc766ef6669657637c8f2d1a9
Omid Jaberi <omid.jaberie@gmail.com>
2026-01-20 17:37:12 +0330

    feat: simple serialization tests

--- tests/unit/test_json.c
+++ tests/unit/test_json.c
@@ -11,6 +11,7 @@ void test_json_text()
     set_json_text_value(&json, "Hello there!");
     assert(json.size == 12);
     assert(strcmp(get_json_text_value(&json), "Hello there!") == 0);
+    destroy_json(&json);
 }
 
 void test_json_array()
@@ -31,6 +32,7 @@ void test_json_array()
     {
         assert(strcmp(get_json_text_value(get_json_array_index(&arr, i)), vals[i]) == 0);
     }
+    destroy_json(&arr);
 }
 
 void test_json_object()
@@ -52,6 +54,58 @@ void test_json_object()
         json_node_t *res = get_json_object_value(&obj, keys[i]);
         assert(strcmp(get_json_text_value(res), vals[i]) == 0);
     }
+    destroy_json(&obj);
+}
+
+void test_serialize_text_json()
+{
+    printf("\ttesting parsing JSON text...\n");
+    json_node_t json;
+    init_json(&json, text);
+    set_json_text_value(&json, "123456");
+    char buffer[128];
+    serialize_json(&json, buffer);
+    assert(strcmp(buffer, "\"123456\"") == 0);
+    destroy_json(&json);
+}
+
+void test_serialize_array_json()
+{
+    printf("\ttesting parsing JSON array...\n");
+    char *vals[] = {"ZERO", "ONE", "TWO", "THREE", "FOUR"};
+    json_node_t arr;
+    init_json(&arr, array);
+    json_node_t element[5];
+    for (int i = 0; i < 5; i++)
+    {
+        init_json(element + i, text);
+        set_json_text_value(element + i, vals[i]);
+        push_json_array(&arr, element + i);
+    }
+    char buffer[1024];
+    serialize_json(&arr, buffer);
+    assert(strcmp(buffer, "[\"ZERO\", \"ONE\", \"TWO\", \"THREE\", \"FOUR\"]") == 0);
+    destroy_json(&arr);
+}
+
+void test_serialize_object_json()
+{
+    printf("\ttesting parsing JSON object...\n");
+    char *keys[] = {"one", "two", "three", "four", "five"};
+    char *vals[] = {"ichi", "nii", "san", "yon", "go"};
+    json_node_t obj;
+    init_json(&obj, object);
+    json_node_t value[5];
+    for (int i = 0; i < 4; i++)
+    {
+        init_json(value + i, text);
+        set_json_text_value(value + i, vals[i]);
+        set_json_object_value(&obj, keys[i], value + i);
+    }
+    char buffer[1024];
+    serialize_json(&obj, buffer);
+    assert(strcmp(buffer, "{one: \"ichi\", two: \"nii\", three: \"san\", four: \"yon\"}") == 0);
+    destroy_json(&obj);
 }
 
 int main()
@@ -60,6 +114,9 @@ int main()
     test_json_text();
     test_json_array();
     test_json_object();
+    test_serialize_text_json();
+    test_serialize_array_json();
+    test_serialize_object_json();
     printf("All tests passed.\n");
     return 0;
 }
