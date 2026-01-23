7bd9a1a5978c9cc62f93615afe6dced15603ade1
Omid Jaberi <omid.jaberie@gmail.com>
2026-01-23 17:00:15 +0330

    refac: malloc in unit tests

--- tests/unit/test_json.c
+++ tests/unit/test_json.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void test_json_text()
 {
@@ -75,12 +76,12 @@ void test_serialize_array_json()
     char *vals[] = {"ZERO", "ONE", "TWO", "THREE", "FOUR"};
     json_node_t arr;
     init_json(&arr, array);
-    json_node_t element[5];
     for (int i = 0; i < 5; i++)
     {
-        init_json(element + i, text);
-        set_json_text_value(element + i, vals[i]);
-        push_json_array(&arr, element + i);
+        json_node_t *child = (json_node_t*)malloc(sizeof(json_node_t));
+        init_json(child, text);
+        set_json_text_value(child, vals[i]);
+        push_json_array(&arr, child);
     }
     char buffer[1024];
     serialize_json(&arr, buffer);
@@ -95,12 +96,12 @@ void test_serialize_object_json()
     char *vals[] = {"ichi", "nii", "san", "yon", "go"};
     json_node_t obj;
     init_json(&obj, object);
-    json_node_t value[5];
     for (int i = 0; i < 4; i++)
     {
-        init_json(value + i, text);
-        set_json_text_value(value + i, vals[i]);
-        set_json_object_value(&obj, keys[i], value + i);
+        json_node_t *value = (json_node_t*)malloc(sizeof(json_node_t));
+        init_json(value, text);
+        set_json_text_value(value, vals[i]);
+        set_json_object_value(&obj, keys[i], value);
     }
     char buffer[1024];
     serialize_json(&obj, buffer);
