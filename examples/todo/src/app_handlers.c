7f561ec6a4ba3a3c0e719754c12bd5666d129b5d
Omid Jaberi <omid.jaberie@gmail.com>
2026-02-22 23:23:50 +0330

    feat: seperated repo logic for get and update

--- examples/todo/src/app_handlers.c
+++ examples/todo/src/app_handlers.c
@@ -8,18 +8,76 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Repo Logic
+BBError get_task_key(const char *task_name, char *buffer)
+{
+    buffer = malloc((strlen(task_name) + 5) * sizeof(char));
+    if (!buffer)
+    {
+        printf("FAIL: malloc\n");
+        return BB_ERROR(BB_ERR_ALLOC, "Failed to malloc.");
+    }
+    sprintf(buffer, "task:%s", task_name);
+    return BB_SUCCESS();
+}
+
+BBError is_task_done(const char *task_name, int *is_done)
+{
+    char *task_key;
+    get_task_key(task_name, task_key);
+    if (BB_FAILED(get_task_key(task_name, task_key)))
+    {
+        return BB_ERROR(BB_ERR_ALLOC, "Failed to malloc.");
+    }
+    char buf[64] = {0};
+    if (persist_load(task_key, buf, sizeof(buf)) != 0)
+    {
+        printf("FAIL: persist_load\n");
+        free(task_key);
+        return BB_ERROR(BB_ERR_BAD_REQUEST, "FAIL: persist_load.");
+    }
+    free(task_key);
+    *is_done = strcmp(buf, "done");
+    return BB_SUCCESS();
+}
+
+BBError mark_task_done(const char *task_name)
+{
+    char *task_key;
+    get_task_key(task_name, task_key);
+    if (BB_FAILED(get_task_key(task_name, task_key)))
+    {
+        return BB_ERROR(BB_ERR_ALLOC, "Failed to malloc.");
+    }
+    char buf[64] = {0};
+    if (persist_load(task_key, buf, sizeof(buf)) != 0)
+    {
+        printf("FAIL: persist_load\n");
+        free(task_key);
+        return BB_ERROR(BB_ERR_BAD_REQUEST, "FAIL: persist_load.");
+    }
+    if (persist_save(task_key, "done", 8) != 0)
+    {
+        printf("FAIL: persist_save\n");
+        free(task_key);
+        return BB_ERROR(BB_ERR_BAD_REQUEST, "Failed to save.");
+    }
+    free(task_key);
+    return BB_SUCCESS();
+}
+
+// Route Handlers:
 BBError add_task(request_t *req, response_t *res)
 {
     http_message_t *msg = &GET_SERVER_REQUEST_MESSAGE(*req);
     LOG_INFO("Add task: %s\n", msg->body);
-    char *task_key = malloc((strlen(msg->body) + 5) * sizeof(char));
-    if (!task_key)
+    char *task_key;
+    get_task_key(msg->body, task_key);
+    if (BB_FAILED(get_task_key(msg->body, task_key)))
     {
-        printf("FAIL: malloc\n");
         set_response_status(res, 500);
         return BB_ERROR(BB_ERR_ALLOC, "Failed to malloc.");
     }
-    sprintf(task_key, "task:%s", msg->body);
     char exists_buf[100];
     if (persist_load(task_key, exists_buf, sizeof(exists_buf)) == 0)
     {
@@ -114,55 +172,41 @@ BBError mark_done(request_t *req, response_t *res)
 {
     const char *task_name = get_request_param(req, "task_name");
     LOG_INFO("Mark done: %s\n", task_name);
-    char *task_key = malloc((strlen(task_name) + 5) * sizeof(char));
-    if (!task_key)
+    BBError err = mark_task_done(task_name);
+    switch (err.code)
     {
-        printf("FAIL: malloc\n");
-        set_response_status(res, 500);
-        return BB_ERROR(BB_ERR_ALLOC, "Failed to malloc.");
+        case BB_OK:
+            set_response_status(res, 200);
+            break;
+        case BB_ERR_BAD_REQUEST:
+            set_response_status(res, 404);
+            break;
+        default:
+            set_response_status(res, 500);
+            break;
     }
-    sprintf(task_key, "task:%s", task_name);
-    char buf[64] = {0};
-    if (persist_load(task_key, buf, sizeof(buf)) != 0)
-    {
-        printf("FAIL: persist_load\n");
-        free(task_key);
-        set_response_status(res, 404);
-        return BB_ERROR(BB_ERR_BAD_REQUEST, "FAIL: persist_load.");
-    }
-    if (persist_save(task_key, "done", 8) != 0)
-    {
-        printf("FAIL: persist_save\n");
-        free(task_key);
-        set_response_status(res, 500);
-        return BB_ERROR(BB_ERR_BAD_REQUEST, "Failed to save.");
-    }
-    free(task_key);
-    return BB_SUCCESS();
+    return err;
 }
 
 BBError get_task(request_t *req, response_t *res)
 {
     const char *task_name = get_request_param(req, "task_name");
     LOG_INFO("Get task: %s\n", task_name);
-    char *task_key = malloc((strlen(task_name) + 5) * sizeof(char));
-    if (!task_key)
+    int is_done;
+    BBError err = is_task_done(task_name, &is_done);
+    switch (err.code)
     {
-        printf("FAIL: malloc\n");
-        set_response_status(res, 500);
-        return BB_ERROR(BB_ERR_ALLOC, "Failed to malloc.");
+        case BB_OK:
+            set_response_status(res, 200);
+            set_response_body(res, is_done ? "done" : "not_done");
+            break;
+        case BB_ERR_BAD_REQUEST:
+            set_response_status(res, 404);
+            break;
+        default:
+            set_response_status(res, 500);
+            break;
     }
-    sprintf(task_key, "task:%s", task_name);
-    char buf[64] = {0};
-    if (persist_load(task_key, buf, sizeof(buf)) != 0)
-    {
-        printf("FAIL: persist_load\n");
-        free(task_key);
-        set_response_status(res, 404);
-        return BB_ERROR(BB_ERR_BAD_REQUEST, "FAIL: persist_load.");
-    }
-    set_response_body(res, buf);
-    free(task_key);
     return BB_SUCCESS();
 }
 
