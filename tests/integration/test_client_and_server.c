e4c4ebba21455f4c9d5c31539f86806f2c95c3f5
Omid Jaberi <omid.jaberie@gmail.com>
2026-03-12 15:10:45 +0330

    refac: unified test for client and server

--- tests/integration/test_client_and_server.c
+++ tests/integration/test_client_and_server.c
@@ -1,11 +1,32 @@
+#include "core/server.h"
 #include "core/client.h"
 #include "core/http.h"
 
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <pthread.h>
 
-int main(void)
+char *response_message = "Hello, Blue-Bird :)";
+
+BBError root_handler(request_t *req, response_t *res)
+{
+    set_response_header(res, "Content-Type", "text/plain");
+    set_response_body(res, response_message);
+    return BB_SUCCESS();
+}
+
+void *server(void* arg)
+{
+    bb_server_t server;
+    init_server(&server, 8080);
+
+    add_route(&server, "GET", "/", root_handler);
+    
+    start_server(&server);
+}
+
+int client()
 {
     bb_client_t client;
     request_t req;
@@ -38,13 +59,26 @@ int main(void)
     printf("Body: %s\n", res.msg.body);
 
     assert(res.status_code == 200);
-    assert(strcmp(res.msg.body, "Hello, Blue-Bird :)") == 0);
+    assert(strcmp(res.msg.body, response_message) == 0);
 
     /* ---- cleanup ---- */
     http_client_close(&client);
     destroy_request(&req);
     destroy_response(&res);
 
-    printf("HTTP client integration test passed.\n");
+    printf("HTTP client and server integration test passed.\n");
+    return 0;
+}
+
+int main()
+{
+    pthread_t thread_id;
+    if (pthread_create(&thread_id, NULL, server, NULL) != 0)
+    {
+        fprintf(stderr, "Error creating server thread\n");
+        return 1;
+    }
+    client();
+    // pthread_join(thread_id, NULL);
     return 0;
 }
