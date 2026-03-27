172ed034218450954b1b5369d1186eca8f2ece02
Omid Jaberi <omid.jaberie@gmail.com>
2026-03-27 15:40:32 +0330

    feat: test_empty_body_req

--- tests/integration/test_client_and_server.c
+++ tests/integration/test_client_and_server.c
@@ -71,8 +71,8 @@ BBError request_body_handler(request_t *req, response_t *res)
 {
     http_message_t *http_msg = &GET_SERVER_REQUEST_MESSAGE(*req);
     set_response_header(res, "Content-Type", "text/plain");
-    char *msg = malloc(strlen(http_msg->body) + 10);
-    sprintf(msg, "body: %s", http_msg->body);
+    char *msg = malloc(http_msg->body_len + 10);
+    sprintf(msg, "body: %s", http_msg->body ? http_msg->body : "");
     set_response_body(res, msg);
     free(msg);
     return BB_SUCCESS();
@@ -405,6 +405,27 @@ void test_req_large_body()
     destroy_response(&res);
 }
 
+void test_empty_body_req()
+{
+    printf("Testing empty body...\n");
+    request_t req;
+    response_t res;
+    init_request(&req);
+    init_response(&res);
+
+    set_request_method(&req, "GET");
+    set_request_url(&req, "/body");
+    set_request_body(&req, "");
+
+    client_request(&req, &res);
+
+    assert(res.status_code == 200);
+    assert(strcmp(res.msg.body, "body: ") == 0);
+
+    destroy_request(&req);
+    destroy_response(&res);
+}
+
 void test_invalid_method()
 {
     printf("Testing unsupported HTTP method...\n");
@@ -444,6 +465,7 @@ int main()
     test_missing_query_param_req();
     test_req_body();
     test_req_large_body();
+    test_empty_body_req();
     test_invalid_method();
 
     printf("HTTP client and server integration tests passed.\n");
