dcc0ae8dc8acf8b3f79c8d42ca3445ed9c63f1f7
Omid Jaberi <omid.jaberie@gmail.com>
2026-04-02 22:20:49 +0330

    refac: seperated functionality by request type

--- src/core/http/request.c
+++ src/core/http/request.c
@@ -1,11 +1,19 @@
 #include "core/http/request.h"
-
+#include "error/assert.h"
 
 void init_request_with_type(request_t *req, request_type type)
 {
     req->type = type;
+    switch (type)
+    {
+    case CLIENT_REQUEST:
+        init_client_request(&req->c_req);
+        break;
+    case SERVER_REQUEST:
+        init_server_request(&req->s_req);
+        break;
+    }
     init_server_request(&req->s_req);
-    init_client_request(&req->c_req);
 }
 
 void init_request(request_t *req) // For Client only
@@ -15,56 +23,71 @@ void init_request(request_t *req) // For Client only
 
 void destroy_request(request_t *req)
 {
-    destroy_client_request(&req->c_req);
-    destroy_server_request(&req->s_req);
+    switch (req->type)
+    {
+    case CLIENT_REQUEST:
+        destroy_client_request(&req->c_req);
+        break;
+    case SERVER_REQUEST:
+        destroy_server_request(&req->s_req);
+        break;
+    }
 }
 
 // Server:
 
 int parse_request(const char *raw, request_t *req)
 {
+    BB_ASSERT(req->type == SERVER_REQUEST, "Invalid request type.");
     return parse_server_request(raw, &req->s_req);
 }
 
 const char *get_request_param(request_t *req, const char *name)
 {
+    BB_ASSERT(req->type == SERVER_REQUEST, "Invalid request type.");
     return get_server_request_param(&req->s_req, name);
 }
 
 int add_request_query_param(request_t *req, const char *key, const char *value)
 {
+    BB_ASSERT(req->type == SERVER_REQUEST, "Invalid request type.");
     return add_server_request_query_param(&req->s_req, key, value);
 }
 
 const char *get_request_query_param(request_t *req, const char *key)
 {
+    BB_ASSERT(req->type == SERVER_REQUEST, "Invalid request type.");
     return get_server_request_query_param(&req->s_req, key);
 }
 
 const char *get_request_header(request_t *req, const char *name)
 {
+    BB_ASSERT(req->type == SERVER_REQUEST, "Invalid request type.");
     return get_server_request_header(&req->s_req, name);
 }
-
 
 // Client:
 
 void set_request_method(request_t *req, const char *method)
 {
+    BB_ASSERT(req->type == CLIENT_REQUEST, "Invalid request type.");
     set_client_request_method(&req->c_req, method);
 }
 
 void set_request_url(request_t *req, const char *url)
 {
+    BB_ASSERT(req->type == CLIENT_REQUEST, "Invalid request type.");
     set_client_request_url(&req->c_req, url);
 }
 
 void set_request_header(request_t *req, const char *name, const char *value)
 {
+    BB_ASSERT(req->type == CLIENT_REQUEST, "Invalid request type.");
     set_client_request_header(&req->c_req, name, value);
 }
 
 void set_request_body(request_t *req, char *body)
 {
+    BB_ASSERT(req->type == CLIENT_REQUEST, "Invalid request type.");
     set_client_request_body(&req->c_req, body);
 }
