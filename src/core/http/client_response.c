6d5dc7917b8414680b914932e9d76c9cd4ac3c9c
Omid Jaberi <omid.jaberie@gmail.com>
2026-04-03 16:08:33 +0330

    feat: utilized message parser in client response parser

--- src/core/http/client_response.c
+++ src/core/http/client_response.c
@@ -25,65 +25,11 @@ int parse_client_response(const char *raw, client_response_t *res)
         return -1;
 
     init_client_response(res);
-
-    /* ---- status line ---- */
-    const char *line_end = strstr(raw, "\r\n");
-    if (!line_end)
+    if (parse_message(raw, &res->msg) != 0)
         return -1;
 
-    char status_line[256];
-    size_t len = line_end - raw;
-    if (len >= sizeof(status_line))
-        len = sizeof(status_line) - 1;
-
-    memcpy(status_line, raw, len);
-    status_line[len] = '\0';
-
-    set_message_start_line(&res->msg, status_line);
-
     /* HTTP/1.1 200 OK */
-    sscanf(status_line, "HTTP/%*s %d", &res->status_code);
-
-    /* ---- headers ---- */
-    const char *p = line_end + 2;
-    while (*p && !(p[0] == '\r' && p[1] == '\n'))
-    {
-        const char *colon = strchr(p, ':');
-        const char *end = strstr(p, "\r\n");
-        if (!colon || !end)
-            return -1;
-
-        char name[128];
-        char value[512];
-
-        size_t name_len = colon - p;
-        if (name_len >= sizeof(name))
-            name_len = sizeof(name) - 1;
-        memcpy(name, p, name_len);
-        name[name_len] = '\0';
-
-        const char *val = colon + 1;
-        while (*val == ' ')
-            val++;
-
-        size_t val_len = end - val;
-        if (val_len >= sizeof(value))
-            val_len = sizeof(value) - 1;
-        memcpy(value, val, val_len);
-        value[val_len] = '\0';
-
-        set_message_header(&res->msg, name, value);
-        p = end + 2;
-    }
-
-    /* ---- body ---- */
-    const char *body = strstr(raw, "\r\n\r\n");
-    if (!body)
-        return 0;
-
-    body += 4;
-    if (*body)
-        set_message_body(&res->msg, body);
+    sscanf(res->msg.start_line, "HTTP/%*s %d", &res->status_code);
 
     return 0;
 }
