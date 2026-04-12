b803615d3e2bb97806007be59291d41120f3a373
Omid Jaberi <omid.jaberie@gmail.com>
2026-04-12 23:30:55 +0330

    feat: sqlite model api header

---
+++ include/persist/model/model_sqlite.h
@@ -0,0 +1,8 @@
+#ifndef BB_OBJECT_PERSIST_SQLITE_H
+#define BB_OBJECT_PERSIST_SQLITE_H
+
+#include "persist/model.h"
+
+const BB_ModelAPI *bb_model_sqlite_api(void);
+
+#endif //BB_OBJECT_PERSIST_SQLITE_H
