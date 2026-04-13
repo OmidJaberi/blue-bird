c21aa50f674311edb20d3ac27b3765b5ce81744a
Omid Jaberi <omid.jaberie@gmail.com>
2026-04-13 21:20:41 +0330

    feat: open and close sqlite model

--- src/persist/model/model_sqlite.c
+++ src/persist/model/model_sqlite.c
@@ -20,29 +20,52 @@ static const char *field_type_to_sql(BB_FieldType type)
     }
 }
 
-static int ensure_table(sqlite3 *db, BB_Schema *schema) {
+static int ensure_table(sqlite3 *db, BB_Schema *schema)
+{
     char sql[1024] = {0};
     strcat(sql, "CREATE TABLE IF NOT EXISTS ");
     strcat(sql, schema->name);
     strcat(sql, " (");
 
-    for (size_t i = 0; i < schema->field_count; i++) {
+    for (size_t i = 0; i < schema->field_count; i++)
+    {
         BB_Field *f = &schema->fields[i];
 
         strcat(sql, f->name);
         strcat(sql, " ");
         strcat(sql, field_type_to_sql(f->type));
 
-        if ((int)i == schema->primary_key_index) {
+        if ((int)i == schema->primary_key_index)
             strcat(sql, " PRIMARY KEY");
-        }
 
-        if (i < schema->field_count - 1) {
+        if (i < schema->field_count - 1)
             strcat(sql, ", ");
-        }
     }
 
     strcat(sql, ");");
 
     return sqlite3_exec(db, sql, NULL, NULL, NULL);
+}
+
+static BB_ModelHandle *sqlite_open(const char *uri)
+{
+    BB_ModelSQLiteHandle *h = malloc(sizeof(*h));
+    if (!h) return NULL;
+
+    if (sqlite3_open(uri, &h->db) != SQLITE_OK)
+    {
+        free(h);
+        return NULL;
+    }
+
+    return (BB_ModelHandle *)h;
+}
+
+static void sqlite_close(BB_ModelHandle *handle)
+{
+    BB_ModelSQLiteHandle *h = (BB_ModelSQLiteHandle *)handle;
+    if (!h) return;
+
+    sqlite3_close(h->db);
+    free(h);
 }
