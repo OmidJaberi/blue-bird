f447e762450ad2f04d2af1f346a133a991c8fc15
Omid Jaberi <omid.jaberie@gmail.com>
2026-04-18 20:10:46 +0330

    feat: model_json handle

---
+++ include/persist/model/model_json.h
@@ -0,0 +1,8 @@
+#ifndef BB_OBJECT_PERSIST_JSON_H
+#define BB_OBJECT_PERSIST_JSON_H
+
+#include "persist/model.h"
+
+const BB_ModelAPI *bb_model_json_api(void);
+
+#endif //BB_OBJECT_PERSIST_JSON_H
---
+++ src/persist/model/model_json.c
@@ -0,0 +1,6 @@
+#include "utils/json.h"
+#include "persist/model/model_json.h"
+
+typedef struct {
+    char *path;
+} BB_ModelJSONHandle;
