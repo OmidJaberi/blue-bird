ff309712c66f0a416019867201ac7f4bd9718624
Omid Jaberi <omid.jaberie@gmail.com>
2026-04-22 20:23:41 +0330

    refac: model based repo

--- include/repo/repo.h
+++ include/repo/repo.h
@@ -1,70 +1,30 @@
 #ifndef BB_REPO_H
 #define BB_REPO_H
 
-#include <stdint.h>
-#include <stddef.h>
-#include "persist/key_val.h"
+#include "persist/model.h"
+#include "persist/schema.h"
 
-/*
- * Repo = record-oriented adapter over persist (KV store)
- *
- * It maps:
- *   (nspace, id) -> key string -> opaque bytes
- *
- * Repo knows NOTHING about:
- *   - HTTP
- *   - JSON
- *   - schemas
- *   - queries
- */
+typedef struct BB_Repo BB_Repo;
 
-typedef struct bb_repo bb_repo_t;
+typedef struct {
+    int (*insert)(BB_Repo *r, void *entity);
+    int (*find_by_id)(BB_Repo *r, void *out, int id);
+    int (*update)(BB_Repo *r, void *entity);
+    int (*remove)(BB_Repo *r, int id);
+} BB_RepoOps;
 
-typedef enum {
-    BB_REPO_OK = 0,
-    BB_REPO_NOT_FOUND,
-    BB_REPO_CONFLICT,
-    BB_REPO_ERROR
-} bb_repo_status_t;
+struct BB_Repo {
+    const BB_ModelAPI *api;
+    BB_ModelHandle *handle;
+    BB_Schema *schema;
 
-/* serialization hooks (provided by application) */
-typedef int (*bb_repo_encode_fn)(
-    const void *record,
-    void       **out_buf,
-    size_t     *out_size
-);
+    BB_RepoOps ops;
+};
 
-typedef int (*bb_repo_decode_fn)(
-    const void *buf,
-    size_t      size,
-    void       *out_record
-);
+/* constructor */
+void bb_repo_init(BB_Repo *r,
+                  const BB_ModelAPI *api,
+                  BB_ModelHandle *handle,
+                  BB_Schema *schema);
 
-/* lifecycle */
-bb_repo_t *bb_repo_create(
-    const char *nspace,
-    bb_repo_encode_fn encode,
-    bb_repo_decode_fn decode
-);
-
-void bb_repo_destroy(bb_repo_t *repo);
-
-/* operations */
-bb_repo_status_t bb_repo_put(
-    bb_repo_t *repo,
-    uint64_t   id,
-    const void *record
-);
-
-bb_repo_status_t bb_repo_get(
-    bb_repo_t *repo,
-    uint64_t   id,
-    void       *out_record
-);
-
-bb_repo_status_t bb_repo_delete(
-    bb_repo_t *repo,
-    uint64_t   id
-);
-
-#endif //BB_REPO_H
+#endif
