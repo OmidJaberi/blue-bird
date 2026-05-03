2ffea44049aaf605b32ce96890108d9d6d8fd310
Omid Jaberi <omid.jaberie@gmail.com>
2026-05-03 20:05:34 +0330

    feat: test duplicate field names

--- tests/unit/test_schema.c
+++ tests/unit/test_schema.c
@@ -36,11 +36,45 @@ static void test_valid_schema()
     assert(bb_schema_validate(&schema) == 0);
 }
 
+static void test_duplicate_field_names()
+{
+    printf("\tTesting duplicate field names...\n");
+    typedef struct {
+        int id;
+    } User;
+
+    BB_Field fields[] = {
+        {
+            "id",
+            BB_FIELD_INT,
+            offsetof(User, id),
+            sizeof(int)
+        },
+        {
+            "id",
+            BB_FIELD_INT,
+            offsetof(User, id),
+            sizeof(int)
+        }
+    };
+
+    BB_Schema schema = {
+        .name = "users",
+        .fields = fields,
+        .field_count = 2,
+        .struct_size = sizeof(User),
+        .primary_key_index = 0
+    };
+
+    assert(bb_schema_validate(&schema) != 0);
+}
+
 int main()
 {
     printf("Running schema tests...\n");
 
     test_valid_schema();
+    test_duplicate_field_names();
 
     printf("All tests passed.\n");
     return 0;
