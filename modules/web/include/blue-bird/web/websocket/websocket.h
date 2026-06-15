acda9741e9372b4cf619b6c4261565dade510dd4
Omid Jaberi <omid.jaberie@gmail.com>
2026-06-15 23:16:40 +0330

    feat: websocket helpers

--- modules/web/include/blue-bird/web/websocket/websocket.h
+++ modules/web/include/blue-bird/web/websocket/websocket.h
@@ -50,6 +50,17 @@ bb_error_t bb_websocket_write_frame(bb_websocket_t *ws, const bb_ws_frame_t *fra
 
 void bb_ws_frame_destroy(bb_ws_frame_t *frame);
 
+// Convenience helpers
+bb_error_t bb_websocket_send_text(bb_websocket_t *ws, const char *text);
+
+bb_error_t bb_websocket_send_binary(bb_websocket_t *ws, const void *data, size_t length);
+
+bb_error_t bb_websocket_send_ping(bb_websocket_t *ws);
+
+bb_error_t bb_websocket_send_pong(bb_websocket_t *ws);
+
+bb_error_t bb_websocket_send_close(bb_websocket_t *ws);
+
 
 #ifdef __cplusplus
 }
