c9f674df88b66116239fb762e78b619aed46a4f3
Omid Jaberi <omid.jaberie@gmail.com>
2026-07-02 16:08:03 +0330

    feat: websocket multiple messages test

--- modules/web/tests/integration/test_websocket_pipeline.c
+++ modules/web/tests/integration/test_websocket_pipeline.c
@@ -102,6 +102,66 @@ static void websocket_echo_test(void)
     bb_runtime_destroy(runtime);
 }
 
+ // Multi Message test
+
+static volatile int messages = 0;
+
+static void _multi_messages_connect_cb(bb_ws_client_t *client, bb_error_t err, void *userdata)
+{
+    (void) err;
+    (void) userdata;
+
+    bb_ws_client_send_text(client, "one");
+    bb_ws_client_send_text(client, "two");
+    bb_ws_client_send_text(client, "three");
+}
+
+bb_error_t _multi_messages_message_cb(bb_ws_context_t *ctx, const bb_ws_message_t *msg)
+{
+    messages++;
+
+    switch(messages)
+    {
+        case 1:
+            assert(strcmp(msg->data, "one") == 0);
+            break;
+
+        case 2:
+            assert(strcmp(msg->data, "two") == 0);
+            break;
+
+        case 3:
+            assert(strcmp(msg->data, "three") == 0);
+            finished = 1;
+            break;
+    }
+
+    return BB_SUCCESS();
+}
+
+static void websocket_multi_message_test(void)
+{
+    printf("\tTesting WebSocket Multiple Messages...\n");
+
+    echo_finished = 0;
+
+    bb_runtime_t *runtime = bb_runtime_create();
+
+    bb_ws_client_t *client = bb_ws_client_create_on_runtime(runtime);
+
+    bb_ws_client_set_message_callback(client, _multi_messages_message_cb, NULL);
+
+    bb_ws_client_connect_async(client, "ws://127.0.0.1:8080/echo", _multi_messages_connect_cb, NULL);
+
+    while (messages < 4)
+    {
+        bb_runtime_tick(runtime);
+    }
+
+    bb_ws_client_destroy(client);
+    bb_runtime_destroy(runtime);
+}
+
 /* ============================================================
  * Main
  * ============================================================ */
@@ -115,6 +175,7 @@ int main(void)
     printf("Testing websocket client/server integration:\n");
 
     websocket_echo_test();
+    websocket_multi_message_test();
 
     finished = 1;
     printf("All websocket integration tests passed.\n");
