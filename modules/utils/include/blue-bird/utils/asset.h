e4758a0ab8f98dbfe9ec730fd00b9d5d06c1c8fd
Omid Jaberi <omid.jaberie@gmail.com>
2026-07-14 19:36:29 +0330

    feat: bb_asset_resolve_path

--- modules/utils/include/blue-bird/utils/asset.h
+++ modules/utils/include/blue-bird/utils/asset.h
@@ -5,6 +5,7 @@
 
 #include "blue-bird/error/error.h"
 
+bb_error_t bb_asset_resolve_path(const char *path, char *buffer, size_t size);
 bb_error_t bb_asset_text_read_all(const char *path, char **buffer, size_t *length);
 
 #endif /* BB_ASSET_H */
--- modules/utils/src/asset.c
+++ modules/utils/src/asset.c
@@ -2,6 +2,104 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#if defined(_WIN32)
+
+#include <windows.h>
+
+#define BB_PATH_SEP '\\'
+#define BB_MAX_PATH MAX_PATH
+
+#else
+
+#include <limits.h>
+#include <unistd.h>
+
+#if defined(__APPLE__)
+#include <mach-o/dyld.h>
+#endif
+
+#define BB_PATH_SEP '/'
+#ifndef PATH_MAX
+#define PATH_MAX 4096
+#endif
+#define BB_MAX_PATH PATH_MAX
+
+#endif
+
+bb_error_t bb_asset_resolve_path(const char *path, char *resolved, size_t size)
+{
+    if (!path || !resolved)
+    {
+        return BB_ERROR(BB_ERR_INTERNAL, "Invalid argument");
+    }
+
+#if defined(_WIN32)
+
+    if ((strlen(path) > 2 && path[1] == ':') || (path[0] == '\\' || path[0] == '/'))
+    {
+        strncpy(resolved, path, size - 1);
+        resolved[size - 1] = '\0';
+        return BB_SUCCESS();
+    }
+
+    DWORD len = GetModuleFileNameA(NULL, resolved, (DWORD)size);
+    if (len == 0 || len >= size)
+    {
+        return BB_ERROR(BB_ERR_IO, "Unable to determine executable path");
+    }
+
+#else
+
+    if (path[0] == '/')
+    {
+        strncpy(resolved, path, size - 1);
+        resolved[size - 1] = '\0';
+        return BB_SUCCESS();
+    }
+
+#if defined(__APPLE__)
+
+    uint32_t len = (uint32_t)size;
+
+    if (_NSGetExecutablePath(resolved, &len) != 0)
+    {
+        return BB_ERROR(BB_ERR_IO, "Unable to determine executable path");
+    }
+
+#else
+
+    ssize_t len = readlink("/proc/self/exe", resolved, size - 1);
+
+    if (len < 0)
+    {
+        return BB_ERROR(BB_ERR_IO, "Unable to determine executable path");
+    }
+
+    resolved[len] = '\0';
+
+#endif
+
+#endif
+
+    char *slash = strrchr(resolved, BB_PATH_SEP);
+    if (!slash)
+    {
+        return BB_ERROR(BB_ERR_IO, "Invalid executable path");
+    }
+
+    *(slash + 1) = '\0';
+
+    if (strlen(resolved) + strlen(path) + 1 > size)
+    {
+        return BB_ERROR(BB_ERR_IO, "Resolved asset path too long");
+    }
+
+    strcat(resolved, path);
+
+    return BB_SUCCESS();
+}
 
 bb_error_t bb_asset_text_read_all(const char *path, char **buffer, size_t *length)
 {
@@ -9,13 +107,22 @@ bb_error_t bb_asset_text_read_all(const char *path, char **buffer, size_t *lengt
     long size;
     size_t bytes_read;
     char *data;
+    char resolved[BB_MAX_PATH];
 
     if (!path || !buffer)
-        return BB_ERROR(BB_ERR_INTERNAL, "Invalid arguement");
+    {
+        return BB_ERROR(BB_ERR_INTERNAL, "Invalid argument");
+    }
+
+    bb_error_t err = bb_asset_resolve_path(path, resolved, sizeof(resolved));
+    if (BB_FAILED(err))
+    {
+        return err;
+    }
 
     *buffer = NULL;
 
-    file = fopen(path, "rb");
+    file = fopen(resolved, "rb");
     if (!file)
         return BB_ERROR(BB_ERR_IO, "File not found");
 
@@ -42,7 +149,7 @@ bb_error_t bb_asset_text_read_all(const char *path, char **buffer, size_t *lengt
     if (!data)
     {
         fclose(file);
-        return BB_ERROR(BB_ERR_ALLOC, "Alloc failed");
+        return BB_ERROR(BB_ERR_ALLOC, "Allocation failed");
     }
 
     bytes_read = fread(data, 1, (size_t)size, file);
