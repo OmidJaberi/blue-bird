2cc24982f7b47d059f6440a4fbe8f06de4ab60d6
Omid Jaberi <omid.jaberie@gmail.com>
2026-08-05 18:23:16 +0330

    feat: bb_task_config_t

--- modules/runtime/include/blue-bird/runtime/task.h
+++ modules/runtime/include/blue-bird/runtime/task.h
@@ -6,10 +6,25 @@ extern "C" {
 #endif
 
 
+typedef enum
+{
+    BB_TASK_COMPLETED,
+    BB_TASK_ERROR,
+    BB_TASK_CANCELLED,
+    BB_TASK_SHUTDOWN,
+} bb_task_result_t;
+
 typedef struct bb_task bb_task_t;
 
 typedef void (*bb_task_cb)(bb_task_t *task, void *userdata);
+typedef void (*bb_task_cleanup_cb)(bb_task_t *task, void *userdata, bb_task_result_t result);
 
+typedef struct
+{
+    bb_task_cb run;
+    void *userdata;
+    bb_task_cleanup_cb cleanup;
+} bb_task_config_t;
 
 int bb_task_is_cancelled(const bb_task_t *task);
 
